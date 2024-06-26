343765d8fbfc3486c4b7668d6d40f648fa40e5f8
NatHan <nathannssilva12@gmail.com>
2024-06-26 12:23:54 -0300

    first commit

---
+++ home.php
@@ -0,0 +1,183 @@
+<?php
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+$select_likes = $conn->prepare("SELECT * FROM `likes` WHERE user_id = ?");
+$select_likes->execute([$user_id]);
+$total_likes = $select_likes->rowCount();
+
+$select_comments = $conn->prepare("SELECT * FROM `comments` WHERE user_id = ?");
+$select_comments->execute([$user_id]);
+$total_comments = $select_comments->rowCount();
+
+$select_bookmark = $conn->prepare("SELECT * FROM `bookmark` WHERE user_id = ?");
+$select_bookmark->execute([$user_id]);
+$total_bookmarked = $select_bookmark->rowCount();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>inicio</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php include 'components/user_header.php'; ?>
+
+<!-- quick select section starts  -->
+
+<section class="quick-select">
+
+   <h1 class="heading">Opções rápidas</h1>
+
+   <div class="box-container">
+
+      <?php
+         if($user_id != ''){
+      ?>
+      <div class="box">
+         <h3 class="title">Curtidas e comentários</h3>
+         <p>total de likes : <span><?= $total_likes; ?></span></p>
+         <a href="likes.php" class="inline-btn">ver curtidas</a>
+         <p>total de comentarios : <span><?= $total_comments; ?></span></p>
+         <a href="comments.php" class="inline-btn">ver comentarios</a>
+         <p>playlist salvas: <span><?= $total_bookmarked; ?></span></p>
+         <a href="bookmark.php" class="inline-btn">ver marcador</a>
+      </div>
+      <?php
+         }else{ 
+      ?>
+      <div class="box" style="text-align: center;">
+         <h3 class="title">Por favor, entre ou se cadastre</h3>
+          <div class="flex-btn" style="padding-top: .5rem;">
+            <a href="login.php" class="option-btn">Entrar</a>
+            <a href="register.php" class="option-btn">Cadastrar</a>
+         </div>
+      </div>
+      <?php
+      }
+      ?>
+
+      <div class="box">
+         <h3 class="title">Top categorias</h3>
+         <div class="flex">
+            <a href="search_course.php?"><i class="fas fa-code"></i><span>Desenvolvimento</span></a>
+            <a href="#"><i class="fas fa-chart-simple"></i><span>negócios</span></a>
+            <a href="#"><i class="fas fa-pen"></i><span>design</span></a>
+            <a href="#"><i class="fas fa-chart-line"></i><span>marketing</span></a>
+            <a href="#"><i class="fas fa-music"></i><span>musica</span></a>
+            <a href="#"><i class="fas fa-camera"></i><span>fotografia</span></a>
+            <a href="#"><i class="fas fa-cog"></i><span>software</span></a>
+            <a href="#"><i class="fas fa-vial"></i><span>ciencia</span></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <h3 class="title">topicos populares/h3>
+         <div class="flex">
+            <a href="#"><i class="fab fa-html5"></i><span>HTML</span></a>
+            <a href="#"><i class="fab fa-css3"></i><span>CSS</span></a>
+            <a href="#"><i class="fab fa-js"></i><span>javascript</span></a>
+            <a href="#"><i class="fab fa-react"></i><span>react</span></a>
+            <a href="#"><i class="fab fa-php"></i><span>PHP</span></a>
+            <a href="#"><i class="fab fa-bootstrap"></i><span>bootstrap</span></a>
+         </div>
+      </div>
+
+      <div class="box tutor">
+         <h3 class="title">Seja um tutor</h3>
+         <p>Entre para o time de professores qualificados</p>
+         <a href="admin/register.php" class="inline-btn">Comece agora</a>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- quick select section ends -->
+
+<!-- courses section starts  -->
+
+<section class="courses">
+
+   <h1 class="heading">Ultimos cursos</h1>
+
+   <div class="box-container">
+
+      <?php
+         $select_courses = $conn->prepare("SELECT * FROM `playlist` WHERE status = ? ORDER BY date DESC LIMIT 6");
+         $select_courses->execute(['active']);
+         if($select_courses->rowCount() > 0){
+            while($fetch_course = $select_courses->fetch(PDO::FETCH_ASSOC)){
+               $course_id = $fetch_course['id'];
+
+               $select_tutor = $conn->prepare("SELECT * FROM `tutors` WHERE id = ?");
+               $select_tutor->execute([$fetch_course['tutor_id']]);
+               $fetch_tutor = $select_tutor->fetch(PDO::FETCH_ASSOC);
+      ?>
+      <div class="box">
+         <div class="tutor">
+            <img src="uploaded_files/<?= $fetch_tutor['image']; ?>" alt="">
+            <div>
+               <h3><?= $fetch_tutor['name']; ?></h3>
+               <span><?= $fetch_course['date']; ?></span>
+            </div>
+         </div>
+         <img src="uploaded_files/<?= $fetch_course['thumb']; ?>" class="thumb" alt="">
+         <h3 class="title"><?= $fetch_course['title']; ?></h3>
+         <a href="playlist.php?get_id=<?= $course_id; ?>" class="inline-btn">ver playlist</a>
+      </div>
+      <?php
+         }
+      }else{
+         echo '<p class="empty">no courses added yet!</p>';
+      }
+      ?>
+
+   </div>
+
+   <div class="more-btn">
+      <a href="courses.php" class="inline-option-btn">saiba mais</a>
+   </div>
+
+</section>
+
+<!-- courses section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+<?php include 'components/footer.php'; ?>
+<!-- footer section ends -->
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+   
+</body>
+</html>
